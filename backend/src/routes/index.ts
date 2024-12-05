79c2a0087a6fb3b0cda002d9efdcc8c3f396a938
Jailton Júnior DEV <jailtonjr99@hotmail.com.br>
2024-12-05 09:39:15 -0300

    Initial configs frontend

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,8 @@
 import {Router} from "express";
-import authRoutes from "./auth.routes";
 import productRoutes from "./product.routes";
 
 const routes = Router();
 
-routes.use("/auth", authRoutes);
 routes.use("/products", productRoutes)
 
 export default routes;
