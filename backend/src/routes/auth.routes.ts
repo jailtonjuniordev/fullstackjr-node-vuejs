1364cf45434b7d733d7e906174cb6fb9b33ebd2f
Jailton Júnior DEV <jailtonjr99@hotmail.com.br>
2024-12-05 09:23:00 -0300

    Backend finalized

--- backend/src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import AuthController from "../controllers/authController";
-
-const authRoutes = Router();
-
-authRoutes.post("/login", new AuthController().userLogin);
-authRoutes.post("/register", new AuthController().userRegister);
-
-export default authRoutes;
